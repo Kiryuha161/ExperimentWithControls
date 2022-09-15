8027f90728821d3bbf675df143177a4f687ff417
kiryuha161@gmail.com <kiryuha161@gmail.com>
2022-09-15 21:07:32 +0300

    Добавил несколько типов ввода чисел

--- ExperimentWithControls/MainWindow.xaml.cs
+++ ExperimentWithControls/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿//*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +23,66 @@ namespace ExperimentWithControls
         public MainWindow()
         {
             InitializeComponent();
+
         }
+
+        private void numberTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            number.Text = numberTextBox.Text; //задаём текст элемента TextBlock(#) так, чтобы он совпадал с текстом элемента TextBox (0). Он изменяется каждый раз, когда
+            //пользователь изменяет текст в TextBox
+        }
+
+        private void numberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out int result);
+            /* 1) Обработчик события вызывается, когда пользователь вводит текст в TextBox, но до обновления TextBox
+             * 2) Специальный метод int.TryParse используется для проверки того, что введённый пользователем текст является числом
+             * 3) Если пользователь ввёл число, e.Handled присваивается true; тем самым вы указываете WPF игнорировать ввод
+             */
+        }
+
+        private void smallSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            number.Text = smallSlider.Value.ToString("0");//значение Slider дробное. Здесь его преобразуют в целое число
+        }
+
+        private void bigSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            number.Text = bigSlider.Value.ToString("000-000-0000");
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton radioButton)
+            {
+                number.Text = radioButton.Content.ToString();
+            }
+        }
+
+        private void myListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (myListBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void readOnlyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (readOnlyComboBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }    
+        }
+
+        private void editableComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is ComboBox comboBox)
+            {
+                number.Text = comboBox.Text;
+            }
+        }
+
+       
     }
 }
